7e40efb5a42f9467799513e9526ecb61164b7890
Vardan <vardan.davtyan.y@tumo.org>
2024-04-29 21:42:36 +0400

    f

--- ai_functionality.py
+++ ai_functionality.py
@@ -7,39 +7,39 @@ import logging
 from scrapper import scrap_website_data
 
 
-client = OpenAI(
-    # defaults to os.environ.get("OPENAI_API_KEY")
-)
-
-def prompt_to_openai_api(messages: list, model: str = 'text-davinci-003'):
-    try:
-        print('Prompt start time:', datetime.now())
-        print(messages)
-        response = client.chat.completions.create(
-            model=model,
-            messages=messages,
-            n=1,
-            temperature=0,
-            max_tokens=1000
-        )
-        print('Prompt end time:', datetime.now())
-        return response.choices[0].text.strip()
-    except Exception as e:
-        print("Exception:", e)
-        return None
-
-
-
-def get_vc_data(article_text: str):
-    openai_messages = [{"role": "system", "content":
-                                                    "Your task is to Extract the following information and show it to the user as a JSON object: VC name, contacts, industries that they invest in, investment rounds that they participate/lead." +
-                                                    " please return only json object"
-                        },
-                    {"role": "user", "content": f"""{article_text}"""}
-                    ]
-    response = prompt_to_openai_api(openai_messages, 'gpt-3.5-turbo')
-    print(response)
-    return response
+#client = OpenAI(
+#    # defaults to os.environ.get("OPENAI_API_KEY")
+#)
+#
+#def prompt_to_openai_api(messages: list, model: str = 'text-davinci-003'):
+#    try:
+#        print('Prompt start time:', datetime.now())
+#        print(messages)
+#        response = client.chat.completions.create(
+#            model=model,
+#            messages=messages,
+#            n=1,
+#            temperature=0,
+#            max_tokens=1000
+#        )
+#        print('Prompt end time:', datetime.now())
+#        return response.choices[0].text.strip()
+#    except Exception as e:
+#        print("Exception:", e)
+#        return None
+#
+#
+#
+#def get_vc_data(article_text: str):
+#    openai_messages = [{"role": "system", "content":
+#                                                    "Your task is to Extract the following information and show it to the user as a JSON object: VC name, contacts, industries that they invest in, investment rounds that they participate/lead." +
+#                                                    " please return only json object"
+#                        },
+#                    {"role": "user", "content": f"""{article_text}"""}
+#                    ]
+#    response = prompt_to_openai_api(openai_messages, 'gpt-3.5-turbo')
+#    print(response)
+#    return response
 
 
 """AI assistant part"""
